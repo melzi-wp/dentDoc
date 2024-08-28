89c88867a14d7fd34053f0724569bcc75728e0df
melzi abdelwahab <melziwahab@gmail.com>
2024-08-28 16:03:59 +0100

    CRUD patient list

--- dentDoc/AddPatient.xaml.cs
+++ dentDoc/AddPatient.xaml.cs
@@ -1,18 +1,5 @@
 ﻿using Npgsql;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using static dentDoc.MainWindow;
 
 namespace dentDoc
 {
@@ -21,54 +8,78 @@ namespace dentDoc
     /// </summary>
     public partial class AddPatient : Window
     {
-    
 
+
+
+        private MainWindow mainWindow;
 
         public AddPatient()
         {
             InitializeComponent();
+           
         }
+
         
         private void SaveBtn(object sender, RoutedEventArgs e)
         {
+            
             string connectionString = "Server=localhost;Port=5432;Database=dental_clinic;Username=postgres;Password=123456;";
             string Nom = txtNom.Text;
             string Prenom = txtPrenom.Text;
-            MainWindow mainWindow = new();
-          
+            string Age = txtAge.Text;
             DateTime? DateNaissance = txtDateNaissance.SelectedDate;
             string Telephone = txtTelephone.Text;
+            
             // ... autres informations du patient
 
             using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
             {
                 connection.Open();
                
-                string sql = "INSERT INTO patient (nom, prenom,date_de_naissance, n_telephone) VALUES (@nom, @prenom,  @date_de_naissance, @n_telephone)";
+                string sql = "INSERT INTO patient (nom, prenom, date_de_naissance, n_telephone,age) VALUES (@nom, @prenom,  @date_de_naissance, @n_telephone, @age)";
                 using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
                 {
-                    command.Parameters.AddWithValue("@nom", Nom);
-                    command.Parameters.AddWithValue("@prenom", Prenom);
-                    command.Parameters.AddWithValue("@date_de_naissance", DateNaissance);
-                    command.Parameters.AddWithValue("@n_telephone", Telephone);
-                    
-                    // ... autres paramètres
-
-                    int rowsAffected = command.ExecuteNonQuery();
-                    
-                    if (rowsAffected > 0)
+                    if (Nom == "" || Prenom == "" || Age == "")
                     {
-                        MessageBox.Show("Patient ajouté avec succès !");
-                        Close();
-                        
+                        MessageBox.Show("Entrer les informations de patient");
                     }
+
                     else
                     {
-                        MessageBox.Show("Erreur lors de l'ajout du patient.");
+                        command.Parameters.AddWithValue("@nom", Nom);
+                        command.Parameters.AddWithValue("@prenom", Prenom);
+                        command.Parameters.AddWithValue("@nom", Age);
+                        command.Parameters.AddWithValue("@nom", Nom);
+                        command.Parameters.AddWithValue("@date_de_naissance", DateNaissance);
+                        command.Parameters.AddWithValue("@n_telephone", Telephone);
+
+                        // ... autres paramètres
+                        
+
+                        int rowsAffected = command.ExecuteNonQuery();
+                    
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Patient ajouté avec succès !");
+                            var mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+                            if (mainWindow != null)
+                            {
+                                mainWindow.ChargerLesPatients();
+                            }
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Erreur lors de l'ajout du patient.");
+                        }
+                        
+                        Close();
                     }
+                   
                 }
             }
         }
+       
 
 
         private void AnnuleAddPatientBtn(object sender, RoutedEventArgs e)
--- dentDoc/MainWindow.cs
+++ dentDoc/MainWindow.cs
@@ -1,5 +1,8 @@
 ﻿using Npgsql;
+using System.Data;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
 
 
@@ -7,7 +10,6 @@ namespace dentDoc
 {
     public partial class MainWindow : Window
     {
-        private string connectionString = "Server=localhost;Port=5432;Database=dental_clinic;Username=postgres;Password=123456;";
 
         public MainWindow()
         {
@@ -15,13 +17,16 @@ namespace dentDoc
             ChargerLesPatients();
 
         }
+
         public class Patient
         {
+            public float Id { get; set; }
             public string Nom { get; set; }
             public string Prenom { get; set; }
             public string NumeroTelephone { get; set; }
         }
 
+        private string connectionString = "Server=localhost;Port=5432;Database=dental_clinic;Username=postgres;Password=123456;";
 
         public void ChargerLesPatients()
         {
@@ -31,24 +36,26 @@ namespace dentDoc
                 using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
                 {
                     connection.Open();
-                    string sql = "SELECT nom, prenom, n_telephone FROM patient";
+                    string sql = "SELECT id_patient, nom, prenom, n_telephone FROM patient";
+
                     using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
                     {
                         using (NpgsqlDataReader reader = command.ExecuteReader())
                         {
                             List<Patient> patients = new List<Patient>();
-                            
+
                             while (reader.Read())
                             {
                                 patients.Add(new Patient
                                 {
-                                    Nom = reader.GetString(0),
-                                    Prenom = reader.GetString(1),
-                                    NumeroTelephone = reader.GetString(2)
+                                    Id = reader.GetFloat(0),
+                                    Nom = reader.GetString(1),
+                                    Prenom = reader.GetString(2),
+                                    NumeroTelephone = reader.GetString(3)
                                 });
-                                 
+
                             }
-                            
+
                             patientsDataGrid.ItemsSource = patients;
                         }
                     }
@@ -60,30 +67,70 @@ namespace dentDoc
             }
         }
 
-            private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+
+        private void DeletePatient(float patientId)
         {
-            Application.Current.Shutdown();
-            
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "DELETE FROM patient WHERE id_patient = @patientId";
+                using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@patientId", patientId);
+                    command.ExecuteNonQuery();
+                }
+            }
         }
+
+        private void DeletePatientButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Êtes-vous sûr de vouloir supprimer ce patient ?", "Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                var selectedPatient = patientsDataGrid.SelectedItem as Patient;
+
+                if (selectedPatient != null)
+                {
+                    float patientId = selectedPatient.Id;
+                    DeletePatient(patientId);
+                    ChargerLesPatients();
+                }
+            }
+        }
+
+            private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+            {
+                Application.Current.Shutdown();
+
+            }
+
+        private void Min_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+
+        }
+
 
         private void AddPatientBtn(object sender, RoutedEventArgs e)
-        {
-            AddPatient addPatient = new();
-          
+            {
+                AddPatient addPatient = new();
 
-           addPatient.Show();
 
+                addPatient.Show();
+
+
+            }
+            private void PatientProfilBtn(object sender, RoutedEventArgs e)
+            {
+                PatientProfil patientProfil = new();
+
+
+                patientProfil.ShowDialog();
+
+
+            }
            
-        }
-        private void PatientProfilBtn(object sender, RoutedEventArgs e)
-        {
-            PatientProfil patientProfil = new();
 
-
-            patientProfil.ShowDialog();
-        
 
         }
-
-    }
-}
+    } 
